d26f6627a352c7acd49f52797e51f1f27e4e898d
Junyi Chai <junyic@princeton.edu>
2015-03-23 21:46:08 -0400

    add evaluation for linear regression

--- chromo/linear_regression.py
+++ chromo/linear_regression.py
@@ -12,21 +12,34 @@ test_Y = db.test_Y()
 
 ## Ordinary linear regression
 from sklearn import linear_model
-clf = linear_model.LinearRegression()
-valid_train_samples = ~np.isnan(train_Y)
-clf.fit(train_X[valid_train_samples,:], train_Y[valid_train_samples])
+import time
 
-valid_test_samples = ~np.isnan(test_Y)
-predicted_Y = clf.predict(test_X)
+start_time = time.time()
+num_tests = 10
+for i in range(num_tests):
+    clf = linear_model.LinearRegression()
+    valid_train_samples = ~np.isnan(train_Y)
+    clf.fit(train_X[valid_train_samples,:], train_Y[valid_train_samples])
+    
+    valid_test_samples = ~np.isnan(test_Y)
+    predicted_Y = clf.predict(test_X)
+end_time = time.time()
+print "predition time %f" %((end_time - start_time)/num_tests)
 pred_score = clf.score(test_X[valid_test_samples,:], test_Y[valid_test_samples])
 
 ## OLR but using log(1-X) to predict log(1-Y)
 import scipy.stats
 valid_train_samples = ~np.isnan(train_Y)
-clf.fit(np.log(1.-train_X[valid_train_samples,:]), 
-        np.log(1.-train_Y[valid_train_samples]))
-
-valid_test_samples = ~np.isnan(test_Y)
-predicted_log1mY = clf.predict(np.log(1-test_X))
-predicted_Y = 1. - np.exp(predicted_log1mY)
+start_time = time.time()
+num_tests = 10
+for i in range(num_tests):
+    clf.fit(np.log(1.-train_X[valid_train_samples,:]), 
+            np.log(1.-train_Y[valid_train_samples]))
+    
+    valid_test_samples = ~np.isnan(test_Y)
+    predicted_log1mY = clf.predict(np.log(1-test_X))
+    predicted_Y = 1. - np.exp(predicted_log1mY)
+end_time = time.time()
+print "predition time %f" %((end_time - start_time)/num_tests)
 pred_score = scipy.stats.pearsonr(predicted_Y[valid_test_samples], test_Y[valid_test_samples])[0]
+print pred_score
---
+++ chromo/regression_evaluation.py
@@ -0,0 +1,26 @@
+# evaulate regression performance
+from sklearn.metrics import r2_score
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_squared_error
+
+valid_test_samples = ~np.isnan(test_Y)
+y_true = test_Y[valid_test_samples]
+y_pred = predicted_Y[valid_test_samples]
+print "r2 = %f" %np.sqrt(r2_score(y_true, y_pred))
+#print "explained variance is %f" %explained_variance_score(y_true, y_pred)
+print "MSE = %f" %np.sqrt(mean_squared_error(y_true, y_pred))
+
+## evaulate classification performance
+import sklearn.metrics
+cutoff_level = 0.7
+ylabel_pred = y_pred > cutoff_level
+ylabel_true = y_true > cutoff_level
+report = sklearn.metrics.classification_report(ylabel_true, ylabel_pred)
+print report
+
+auc = sklearn.metrics.roc_auc_score(ylabel_true, y_pred)
+print "AUC = %f" %auc
+
+fpr, tpr, thresholds = sklearn.metrics.roc_curve(ylabel_true, y_pred, pos_label=1)
+plt.plot(fpr, tpr)
+plt.show()
